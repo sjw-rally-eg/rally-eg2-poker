02481a1a6ca62dcc0ec97428c764e3d09cadbed6
sjw_rally_eg <sjw_rally_eg@yahoo.com>
2012-07-31 21:40:10 -0600

    got rid of unused test, and added test for 'empty hand' bug

--- test/test_poker_hand.js
+++ test/test_poker_hand.js
@@ -1,22 +1,9 @@
 describe("poker hand", function() {
 
-  var hand, card, card_str;
-
-  describe("valid cards", function() {
-    it("creates instance OK", function() {
-      hand = new PokerHand();
-      expect( hand ).toBeDefined();
-
-      card_str = "Ad";
-      card = Card.get_instance( card_str );
-      expect( card ).toBeDefined();
-      hand.add( card );
-
-      card_str = "10h";
-      card = Card.get_instance( card_str );
-      expect( card ).toBeDefined();
-      hand.add( card );
-    });
+  // NOTE: all other hand tests are done in poker hand ranker
+  it("throws correct exception for empty input", function() {
+    expect( function(){ new PokerHand(''); } ).toThrow(
+      PokerHandRanker.NumberOfCardsError );
   });
 
 });
